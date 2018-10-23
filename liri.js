32dec2b4ec15909a08f8638cd22f3f5823c70665
Hayden <mhaydenestes@gmail.com>
2018-10-23 18:14:09 -0400

    beginning functions skeleton

--- liri.js
+++ liri.js
@@ -2,19 +2,37 @@ require("dotenv").config();
 var keys = require(keys.js);
 var spotify = new Spotify(keys.spotify);
 
-switch (process.argv) {
-    case ('concert-this'):
+switch (process.argv[2]) {
+    case ('concert-this' | 'concerts' | 'concert' | 'bands-in-town'):
     concert();
     break;
 
-    case("spotify-this-song"):
+    case('spotify-this-song' | 'spotify' | 'song' | 'this-song'):
     spotify();
     break;
 
-    case ('movie-this'):
+    case ('movie-this' | 'movie' | 'movies' | 'this-movie'):
     movie();
     break;
 
     case ('do-what-it-says'):
     command();
+};
+
+function spotify() {
+    var song = process.argv[3];
+    var spotifyURL = `${song}`
+
+};
+
+function concert() {
+
+};
+
+function movie() {
+
+};
+
+function command() {
+
 };
